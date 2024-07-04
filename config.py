79a77bae63633479fa4c4e2f5cfbaccc838ca7d0
preethamkamishetty <kamishettypreetham@gmail.com>
2024-07-04 16:54:03 +0530

    env

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config:
-    MONGO_URI = "mongodb+srv://preetham:37mDBDtI7YqBl6yq@projectx.wwbdqn9.mongodb.net/ProjectX?retryWrites=true&w=majority&appName=ProjectX"
-    SECRET_KEY = "ABC"
+    SECRET_KEY = os.getenv('SECRET_KEY', 'ABC')
+    MONGO_URI = os.getenv('MONGO_URI', 'mongodb+srv://preetham:37mDBDtI7YqBl6yq@projectx.wwbdqn9.mongodb.net/ProjectX?retryWrites=true&w=majority&appName=ProjectX')
